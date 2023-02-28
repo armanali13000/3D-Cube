25923e2f0487c69202a0c34151d0fd3d5a47fde4
armanali13000 <arman.ali.13000@gmail.com>
2023-03-01 00:21:59 +0530

    second Update

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -1,19 +1,13 @@
 ﻿using UnityEngine;
-namespace UnityEngine
-{
     
-
-
-
 public class FollowPlayer : MonoBehaviour
 {
     
    public Transform player; 
    public Vector3 offset;
-
     void Update()
     {
         // Debug.Log(player.position);
         transform.position = player.position + offset; 
     }
-}}
+}
